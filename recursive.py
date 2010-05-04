bfc076f23a644590d0058de77cb8e1ae05450e77
Leon Fedotov <duejohn@gmail.com>
2010-05-04 16:40:57 +0300

    erAN

--- recursive.py
+++ recursive.py
@@ -6,6 +6,7 @@ def drawSerp(dept = 10, drawChar = ".", lastLine = []):
 				newLine.append(1)
 			else :
 				newLine.append(lastLine[x-1]+lastLine[x])
-				
+				 
 		print "%s".rjust((dept+len(lastLine)-len(newLine)+2)*len(drawChar)) % (" "*len(drawChar)).join([drawChar if i%2 else (" "*len(drawChar)) for i in newLine])
 		drawSerp(dept-1, drawChar, newLine)  
+		
